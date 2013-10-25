99cf55d0786951f2317aef2210c6c78bcbb8f752
Jon Andersen <jon.andersen88@gmail.com>
2013-10-25 21:34:40 +0200

    Heroku new build

--- client/main.js
+++ client/main.js
@@ -2,6 +2,7 @@ Meteor.subscribe('destinations');
 
 Meteor.subscribe('regions');
 
+
 //Deps.autorun(function () {
 //    Meteor.subscribe('singlePost', Session.get('currentPostId'));
 //    Meteor.subscribe('comments', Session.get('currentPostId'));
